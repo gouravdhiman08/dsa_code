872004ec3bcbacb9956379167b45b873df827d24
Gourav Dhiman <techstudy.gourav@gmail.com>
2024-05-30 09:58:41 +0530

    void show function added in stack...

--- stack.c
+++ stack.c
@@ -21,10 +21,25 @@ void pop() {
     }
 }
 
+void show(){
+    if (top==-1)
+    {
+        printf("Stack is empty");
+    }else{
+        printf("Elements are in stack");
+        for (int i = 0; i <=top; i++)
+        {
+            printf("\n%d ",stack[i]);
+        }
+        
+    }
+    
+}
+
 int main() {
     int choice;
     while (1) {
-        printf("\nPress 1 to add element to stack\nPress 2 to delete element from stack\nPress 3 to exit\n");
+        printf("\nPress 1 to add element to stack\nPress 2 to delete element from stack\nPress 3 to show\n\nPress 4 to exit");
         scanf("%d", &choice);
 
         if (choice == 1) {
@@ -35,8 +50,11 @@ int main() {
         } else if (choice == 2) {
             pop();
         } else if (choice == 3) {
+            show();
+        }else if (choice == 4) {
             break;
-        } else {
+        } 
+        else {
             printf("Wrong choice\n");
         }
     }
